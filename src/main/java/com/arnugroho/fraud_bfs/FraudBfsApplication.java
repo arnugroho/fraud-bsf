38400e09f2e2256872f15ab1f06d169ba6bd0974
arnugroho <nugroho.akbar.r@gmail.com>
2024-10-04 05:53:52 +0700

    init project

---
+++ src/main/java/com/arnugroho/fraud_bfs/FraudBfsApplication.java
@@ -0,0 +1,13 @@
+package com.arnugroho.fraud_bfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudBfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudBfsApplication.class, args);
+	}
+
+}
